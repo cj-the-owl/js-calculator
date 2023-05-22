39ab61f275160f651a0f38a11c1c1d31560a3fa7
cj-the-owl <lodewykcarynjayn@gmail.com>
2023-05-22 12:44:50 +0200

    last commit

--- script/script.js
+++ script/script.js
@@ -1,4 +1,19 @@
-let button = document.querySelectorAll('button') 
+function display(val) {
+    document.getElementById('answer').value+=val
+    return value
+};
+
+function equals() {
+    let x = document.getElementById('answer').value
+    let y = eval(x);
+    document.getElementById('answer').value = y
+    return y
+}
+
+function AC() {
+    document.getElementById('answer').value=""
+    return null
+}
 
 
 
